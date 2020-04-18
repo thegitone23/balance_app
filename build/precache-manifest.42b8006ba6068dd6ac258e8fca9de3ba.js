3056588d1039dbf25c9bdd90062428b16248f563
Mayank Gautam <mayankgautam98@gmail.com>
2020-04-18 14:37:37 +0530

    added build folder

---
+++ build/precache-manifest.42b8006ba6068dd6ac258e8fca9de3ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef36570b20e3c2f2a77dcb287e8695e6",
+    "url": "/balance_app/index.html"
+  },
+  {
+    "revision": "fcd67abac2108a182000",
+    "url": "/balance_app/static/css/main.4e9bc4c2.chunk.css"
+  },
+  {
+    "revision": "cf44b353ca81c7a83405",
+    "url": "/balance_app/static/js/2.99913136.chunk.js"
+  },
+  {
+    "revision": "ad61e5a3703d8af976e070e9896b739a",
+    "url": "/balance_app/static/js/2.99913136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd67abac2108a182000",
+    "url": "/balance_app/static/js/main.b6ad06c0.chunk.js"
+  },
+  {
+    "revision": "88d3489ddba44628ce9e",
+    "url": "/balance_app/static/js/runtime-main.f85375f7.js"
+  }
+]);
